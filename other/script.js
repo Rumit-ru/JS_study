b0417ec97253112d80c420db46b5b1d2893c7195
Rumit <timurpolushkin.28@gmail.com>
2020-01-20 20:21:33 +0300

    доработал ф-л, отформатировал код

--- other/script.js
+++ other/script.js
@@ -1,27 +1,35 @@
-let box = document.getElementById('box');
-let btn = document.getElementsByTagName('button');
-let circle = document.getElementsByClassName('circle');
+"use strict";
 
-box.style.backgroundColor = 'blue'; // обратились к стилям и меняем цвет квадрата на голубой
-btn[1].style.borderRadius = '100%'; // изменили форму второй кнопки
+let btn = document.querySelectorAll('button'),
+	wrap = document.querySelector('.wrapper'),
+	link = document.querySelector('a');
 
-circle[0].style.backgroundColor = 'yellow'; // изменили цвет элемента, выбрав его из псевдомассива
-circle[1].style.backgroundColor = 'green';
+// btn[0].onclick = function() {
+//     alert('You are push on the first button!');
+// };
 
-let div = document.createElement('div'); // создали новый элемент с помощью метода createElement
-    text = document.createTextNode('hello world'); // создание тестовой ноды без оболочки cerateTextNode
+// btn[0].addEventListener('click', function(event) {
+//     // let target = event.target;
+//     // target.style.display = 'none';  
+//     console.log('Произошло событие: ' + event.type + 'на элементе ' + event.target); 
+// });
 
-div.classList.add('black');
-
-//div.innerHTML = '<h1>Hello World!</h1>' добавляет на странице любой элемент в html код
-div.textContent = 'hello world!'; // добавляет на страницу исключительно текст!
-
-
-
-//  document.body.appendChild(div); // добавили элемент в конец тега body
-    document.body.insertBefore(div, circle[0]); // добавили элемент перед тегом circle, указав порядковый номер элемента
-    document.body.removeChild(circle[2]); // удалили элемент с помощью тега removeChild
-    document.body.replaceChild(btn[1], circle[0]); // заменили circle[0] на btn[1]
+// wrap.addEventListener('click', function() {
+//     console.log('Произошло событие: ' + event.type + 'на элементе ' + event.target); 
+// });
 
 
-console.log(div);
+link.addEventListener('click', function(event) {
+	event.preventDefault();
+    console.log('Произошло событие: ' + event.type + 'на элементе ' + event.target);
+});
+
+btn.forEach(function(item) {
+	item.addEventListener('mouseleave', function() {
+		console.log('Mouse leave nearest of btn!');
+	});
+});
+
+// btn[0].addEventListener('mouseenter', function() {
+//     alert('You pointed to the first button!');
+// });
